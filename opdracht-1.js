0f9a83c9dd38629533cccd1e790cca4bef4bf966
jeffreyheideman <jeffrey_heideman@hotmail.com>
2023-04-17 07:45:12 +0200

    opdracht 1 afgerond

--- opdracht-1.js
+++ opdracht-1.js
@@ -1,8 +1,26 @@
-import inventory
-
 //Opdracht 1a
 const tvTypes = inventory.map((tvType) => {
     return tvType.type;
 });
 
 console.log(tvTypes)
+
+
+//opdracht 1b
+const soldOutTvs = inventory.filter((soldOutTv)=> {
+return soldOutTv.sold === soldOutTv.originalStock
+});
+
+console.log(soldOutTvs)
+
+//Opdracht 1c
+const ambiLightTvs = inventory.filter((ambiLighttv) => {
+    return ambiLighttv.options.ambiLight === true
+});
+
+console.log(ambiLightTvs)
+
+//Opdracht 1d
+const sort = inventory.sort((a, b) => a.price - b.price)
+
+console.log(sort)
